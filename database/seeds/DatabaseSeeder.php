b3310f6a18c73fc4d2e7d78449e2e50094a5e85d
leep0o <genesboard@gmail.com>
2020-01-30 20:14:57 +0300

    added Company, Positions models -a

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            PositionsTableSeeder::class,
+            UsersTableSeeder::class,
+            CompaniesTableSeeder::class,
+        ]);
     }
 }
